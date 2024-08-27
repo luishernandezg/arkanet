af65c3c30c9652d417cab886fcfa14045be6a0e9
Luis Hernandez <luis.guaicaipuro.h.a@gmail.com>
2024-08-27 18:37:18 -0400

    character move animation

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -3,12 +3,23 @@ extends CharacterBody2D
 
 @export var speed = 600.0
 
+@onready var playback: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
+
 func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement
 	var directionX := Input.get_axis("move_left", "move_right")
 	var directionY := Input.get_axis("move_up", "move_dowm")
 	
+	if(velocity == Vector2.ZERO):
+		playback.start('RESET')
+		
+	if(directionX < 0):
+		playback.travel('move_left')
+		
+	if(directionX > 0):
+		playback.travel('move_right')
+		
 	velocity.x = directionX * speed
 	velocity.y = directionY * speed
 
